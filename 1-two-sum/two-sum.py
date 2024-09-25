fa1fa6bf63d8e61cdb63a4b76eaca42a30103249
andche742 <67716795+andche742@users.noreply.github.com>
2024-09-25 13:38:15 -0700

    Time: 32 ms (93.85%) | Memory: 12.4 MB (75.39%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,7 @@ class Solution(object):
             if target - nums[i] in seenNum:
                 return [seenNum[target - nums[i]], i]
             seenNum[nums[i]] = i
-        return []
+            # put item in map in format number: index in nums
         """
         :type nums: List[int]
         :type target: int
