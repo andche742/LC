02e65c9f77a6fbe5ab5cf59feffdde791017e123
andche742 <67716795+andche742@users.noreply.github.com>
2024-09-30 11:37:48 -0700

    Time: 70 ms (76.35%) | Memory: 15.8 MB (65.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,10 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        sorts = [] # list for sorted words
-        groups = [] # output
-        for word in strs: # if word is in sorted, its an anagram: add it to group
-            if (sorted(word) in sorts):
-                groups[sorts.index(sorted(word))].append(word)
-            else: # otherwise make new sorts element for it and new group
-                groups.append([word])
-                sorts.append(sorted(word))
-        return groups
+        anagramMap = defaultdict(list)
 
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+        for word in strs:
+            sortedWord = ''.join(sorted(word))
+            anagramMap[sortedWord].append(word)
+        
+        return list(anagramMap.values())
         
