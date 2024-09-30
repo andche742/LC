cda2faf47b10a2e6013b450d824a731eeb281985
andche742 <67716795+andche742@users.noreply.github.com>
2024-09-30 12:10:48 -0700

    Time: 78 ms (53.16%) | Memory: 14.7 MB (96.66%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,8 +1,8 @@
-from collections import Counter
+from collections import Counter # funny tool
 class Solution(object):
     def topKFrequent(self, nums, k):
-        counter = Counter(nums)
-        return  sorted(counter, key=counter.get, reverse=True)[:k]
+        counter = Counter(nums) # returns dict {number: number of that number}
+        return  sorted(counter, key=counter.get, reverse=True)[:k] # return keys with top values
 
         """
         :type nums: List[int]
