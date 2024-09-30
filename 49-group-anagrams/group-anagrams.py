25ad2ab41b17c3275ff138e57f2878b7411c759f
andche742 <67716795+andche742@users.noreply.github.com>
2024-09-30 11:42:04 -0700

    Time: 78 ms (50.33%) | Memory: 15.8 MB (56.65%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        anagramMap = defaultdict(list)
+        anagramMap = defaultdict(list) # create map, if key not found, create empty array 
 
         for word in strs:
-            sortedWord = ''.join(sorted(word))
-            anagramMap[sortedWord].append(word)
+            sortedWord = ''.join(sorted(word)) # create sorted string of letters
+            anagramMap[sortedWord].append(word) # add word to map, if not found, creates new key, if found, adds to existing
         
         return list(anagramMap.values())
         
