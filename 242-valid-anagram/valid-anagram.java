b646ded5773e6297e35dfb43c516c54e40b00ad2
andche742 <67716795+andche742@users.noreply.github.com>
2024-10-03 23:45:22 -0700

    Time: 14 ms (29.85%) | Memory: 45.1 MB (5.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,27 +3,23 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Integer> sMap = new HashMap();
-        HashMap<Character, Integer> tMap = new HashMap();
-        for (int i = 0; i < s.length(); i++) {
-            if (sMap.containsKey(s.charAt(i))) { // populate sMap
-                sMap.put(s.charAt(i), sMap.get(s.charAt(i)) + 1);
-            }
-            else {
-                sMap.put(s.charAt(i), 1);
-            }
+
+        HashMap<Character, Integer> letters = new HashMap();
+
+        for (char x : s.toCharArray()) {
+            letters.put(x, letters.getOrDefault(x, 0) + 1);
         }
 
-        
-        for (int i = 0; i < t.length(); i++) {
-            if (tMap.containsKey(t.charAt(i))) { // populate tMap
-                tMap.put(t.charAt(i), tMap.get(t.charAt(i)) + 1);
-            }
-            else {
-                tMap.put(t.charAt(i), 1);
-            }
+        for (char x : t.toCharArray()) {
+            letters.put(x, letters.getOrDefault(x, 0) - 1);
         }
 
-        return sMap.equals(tMap);
+        for (int val : letters.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }    
+
+        return true;
     }
 }
