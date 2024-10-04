ed88e16033407112be0c8a5e3a22c818c68a6d49
andche742 <67716795+andche742@users.noreply.github.com>
2024-10-03 19:18:51 -0700

    Time: 39 ms (33.41%) | Memory: 12.7 MB (51.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if (len(s) != len(t)): # if length if diff obv not anagram
+        sSorted = sorted(s)
+        tSorted = sorted(t)
+        if sSorted == tSorted:
+            return True
+        else:
             return False
-        
-        sDict = {} # dictionary to hold # of each letter
-        tDict = {}
-        for i in range(len(s)):
-            sDict[s[i]] = 1 + sDict.get(s[i], 0) # count = 1 + existing count
-            tDict[t[i]] = 1 + tDict.get(t[i], 0)
-        return sDict == tDict # return whether they are equal or not
         """
         :type s: str
         :type t: str
