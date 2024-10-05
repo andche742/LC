ee1b60a7192dea7034c285892505f04b0e14f1dd
andche742 <67716795+andche742@users.noreply.github.com>
2024-10-05 00:14:17 -0700

    Time: 14 ms (19.46%) | Memory: 44.6 MB (42.87%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,18 +1,22 @@
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
+        Set<String> seen = new HashSet<>();
+        
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                // char num = board[i][j];
                 if (board[i][j] != '.') {
-                    if (!seen.add(i + "row" + board[i][j]) ||
-                    !seen.add(j + "column" + board[i][j]) || 
-                    !seen.add(i/3+ "-" + j/3 + "box" + board[i][j]))  {
+                    char num = board[i][j];
+                    // Instead of concatenating strings, create a unique key using the number in row, column, and box
+                    if (!seen.add(num + " in row " + i) || 
+                        !seen.add(num + " in col " + j) || 
+                        !seen.add(num + " in box " + i/3 + "-" + j/3)) {
                         return false;
                     }
                 }
             }
         }
+        
         return true;
     }
 }
